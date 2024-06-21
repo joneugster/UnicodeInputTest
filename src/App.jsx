c5c3ce6bd20ebd0714663836ce849c7998e14598
Jon Eugster <eugster.jon@gmail.com>
2024-06-21 12:35:55 +0200

    minimise

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import UnicodeInput from './UnicodeInput'
 
 function App() {
-  const [content, setContent] = useState("(nothing)")
-
   return (
     <>
       <div>
@@ -20,12 +18,6 @@ function App() {
       <h1>Unicode Input Test</h1>
       <div className="card">
         <UnicodeInput />
-        <p>
-          The content of the input is.
-        </p>
-        <pre>
-        <code>{content}</code>
-        </pre>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
--- src/UnicodeInput.tsx
+++ src/UnicodeInput.tsx
@@ -1,14 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import {
-  AbbreviationConfig,
-  AbbreviationProvider,
-  AbbreviationRewriter,
-  AbbreviationTextSource,
-  Change,
-  Range,
-  SelectionMoveMode,
-} from '@leanprover/unicode-input'
-
+import { AbbreviationConfig } from '@leanprover/unicode-input'
 import { InputAbbreviationRewriter } from '@leanprover/unicode-input-component'
 import './UnicodeInput.css'
 
@@ -18,62 +9,19 @@ const FALLBACK_CONFIG: AbbreviationConfig = {
   eagerReplacementEnabled: true
 }
 
-async function handeBeforeInput (ev: Event) {
-  // const inputEvent = ev as InputEvent
-  // const targetRange: StaticRange | undefined = inputEvent.getTargetRanges()[0]
-  // if (targetRange === undefined) {
-  //     return
-  // }
-  // const range = computeTextRangeFromNodeRange(
-  //     textInput,
-  //     { node: targetRange.startContainer, offset: targetRange.startOffset },
-  //     { node: targetRange.endContainer, offset: targetRange.endOffset },
-  // )
-  // if (range === undefined) {
-  //     return
-  // }
-  // const newText = inputEvent.data ?? ''
-  // const change: Change = { range, newText }
-  // this.rewriter.changeInput([change])
-}
-
-const UnicodeInput: React.FC<{ config?: AbbreviationConfig }> = ({config=FALLBACK_CONFIG}) => {
+const UnicodeInput: React.FC<{
+  config?: AbbreviationConfig
+}> = ({config=FALLBACK_CONFIG}) => {
   const inputRef = useRef<HTMLDivElement>(null)
-  const [abbrevProvider, setAbbrevProvider] = useState<AbbreviationProvider>()
-  const [abbrevRewriter, setAbbrevRewriter] = useState<AbbreviationRewriter>()
-
-  // TODO
-  const textSource: AbbreviationTextSource = {
-    replaceAbbreviations: (changes) => {return new Promise((resolve)=>{return true})},
-    selectionMoveMode: () => ({
-      kind: 'OnlyMoveCursorSelections',
-      updateUnchangedSelections: true}),
-    collectSelections: () => ([]),
-    setSelections: (selections) => Promise.resolve
-  }
+  const [_rewriter, setRewriter] = useState<InputAbbreviationRewriter>()
 
   useEffect(() => {
-    let provider = new AbbreviationProvider(config)
-    setAbbrevProvider(provider)
-    let rewriter = new AbbreviationRewriter(config, provider, textSource)
-    setAbbrevRewriter(rewriter)
-
-
-    // let abbrevRewriter: InputAbbreviationRewriter = new InputAbbreviationRewriter(config, inputRef.current!)
+    let rewriter = new InputAbbreviationRewriter(config, inputRef.current!)
+    setRewriter(rewriter)
   }, [config])
 
-  useEffect(() => {
-    const div = inputRef.current!
-    // subscribe event
-    div.addEventListener("beforeinput", handleOnScroll);
-    return () => {
-      // unsubscribe event
-      div.removeEventListener("scroll", handleOnScroll);
-    };
-  }, []);
-
   return <>
-    <div ref={inputRef} className="unicode-input" contentEditable="true"></div>
+    <div ref={inputRef} className="textinput" id="query" contentEditable="true" autoFocus={true}></div>
   </>
 }
 
